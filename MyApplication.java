9995d889ad3c6d90703aad3efb51761b1e179401
Swapna-SpringPeople <swapnaspringjavascript@gmail.com>
2017-03-27 12:59:45 +0530

    Spring DI Using annotations and Constructor

---
+++ MyApplication.java
@@ -0,0 +1,28 @@
+package com.sakha.spring.di.consumer;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.sakha.spring.di.services.MessageService;
+
+@Component
+public class MyApplication{
+
+	@Autowired
+	public MyApplication(MessageService serv) {
+		super();
+		this.service = serv;
+	}
+	public boolean processMessage(String msg, String rec){
+		//some magic like validation, logging etc
+		return this.service.sendMessage(msg, rec);
+	}
+	@Autowired
+	private MessageService service;
+	
+	
+
+	
+	
+
+}
