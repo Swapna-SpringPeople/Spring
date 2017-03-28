512e4abe8f1a3281614c436236cb3af4907ab79e
Swapna-SpringPeople <swapnaspringjavascript@gmail.com>
2017-03-28 12:49:53 +0530

    SpringRest

---
+++ CountryService.java
@@ -0,0 +1,43 @@
+package com.sakha.service;
+
+
+
+import java.util.List;
+import com.sakha.dao.CountryDAO;
+import com.sakha.model.Country;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service("countryService")
+public class CountryService {
+
+ @Autowired
+ CountryDAO countryDao;
+ 
+ @Transactional
+ public List getAllCountries() {
+  return countryDao.getAllCountries();
+ }
+
+ @Transactional
+ public Country getCountry(int id) {
+  return countryDao.getCountry(id);
+ }
+
+ @Transactional
+ public void addCountry(Country country) {
+  countryDao.addCountry(country);
+ }
+
+ @Transactional
+ public void updateCountry(Country country) {
+  countryDao.updateCountry(country);
+
+ }
+
+ @Transactional
+ public void deleteCountry(int id) {
+  countryDao.deleteCountry(id);
+ }
+}
